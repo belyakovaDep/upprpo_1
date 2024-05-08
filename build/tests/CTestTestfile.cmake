54bf3fb234a2906b4865025cf4f95c9cc7b05815
d.belyakova1 <d.belyakova1@g.nsu.ru>
2024-05-08 20:37:38 +0700

    dfg

--- build/tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aboba/Загрузки/UPPRPO/tests
-# Build directory: /home/aboba/Загрузки/UPPRPO/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/aboba/Загрузки/UPPRPO/build/tests/hello_test2[1]_include.cmake")
